f03a485d1d81decc1ba7a8f74e3a5b884c8adcf0
James Costello <james.m.costello@protonmail.com>
2024-07-24 10:31:16 -0700

    fix error, database.go

--- server/utils/database.go
+++ server/utils/database.go
@@ -10,7 +10,7 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&sslmode=%s",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		url.QueryEscape(os.Getenv("DB_PASSWORD")),
 		os.Getenv("DB_HOST"),
@@ -19,3 +19,4 @@ func ConnectDB() (*gorm.DB, error) {
 	)
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
+
